a6cc2f274c92f154b4cdb11cbffff3c934822c7c
mipesync <arhe414@gmail.com>
2023-08-20 00:27:39 +0300

    feature: Создал базовые сущности

---
+++ LasmartTestContext.Domain/Comment.cs
@@ -0,0 +1,29 @@
+﻿namespace LasmartTestContext.Domain;
+
+/// <summary>
+/// Класс комментария, закреплённого за точкой
+/// </summary>
+public class Comment
+{
+    /// <summary>
+    /// Идентифкатор комментария
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Текст комментария
+    /// </summary>
+    public string Text { get; set; } = null!;
+    /// <summary>
+    /// Цвет фона комментария в формате hex
+    /// </summary>
+    public string? BackgroundColor { get; set; } 
+    /// <summary>
+    /// Идентификатор точки, за которой закреплён комментарий
+    /// </summary>
+    public int PointId { get; set; }
+
+    /// <summary>
+    /// Точка, за которой закреплён комментарий
+    /// </summary>
+    public Point Point { get; set; } = null!;
+}
---
+++ LasmartTestContext.Domain/Point.cs
@@ -0,0 +1,33 @@
+﻿namespace LasmartTestContext.Domain;
+
+/// <summary>
+/// Класс точки, отображаемой на доске
+/// </summary>
+public class Point
+{
+    /// <summary>
+    /// Идентификатор точки
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Положение точки по X
+    /// </summary>
+    public float X { get; set; } = 0f;
+    /// <summary>
+    /// Положение точки по Y
+    /// </summary>
+    public float Y { get; set; } = 0f;
+    /// <summary>
+    /// Радиус точки
+    /// </summary>
+    public int Radius { get; set; } = 10;
+    /// <summary>
+    /// Цвет точки в формате hex
+    /// </summary>
+    public string Color { get; set; } = null!;
+    
+    /// <summary>
+    /// Список комментариев под точкой
+    /// </summary>
+    public List<Comment>? Comments { get; set; }
+}
