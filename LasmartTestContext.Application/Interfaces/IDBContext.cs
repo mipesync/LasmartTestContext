06c271dfd5f9d26686c2eaed09a3400ed6b0d310
mipesync <arhe414@gmail.com>
2023-08-20 00:30:36 +0300

    feature: Добавил контекст базы данных

---
+++ LasmartTestContext.Application/Interfaces/IDBContext.cs
@@ -0,0 +1,25 @@
+﻿using LasmartTestContext.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LasmartTestContext.Application.Interfaces;
+
+/// <summary>
+/// Интерфейс, описывающий контекст базы данных
+/// </summary>
+public interface IDBContext
+{
+    /// <summary>
+    /// Получить / установить список точек
+    /// </summary>
+    DbSet<Point>? Points { get; set; } 
+    /// <summary>
+    /// Получить / установить список комментариев
+    /// </summary>
+    DbSet<Comment>? Comments { get; set; }
+
+    /// <summary>
+    /// Ассинхронно сохраняет внесённые изменения
+    /// </summary>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ LasmartTestContext.Persistence/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using LasmartTestContext.Application.Interfaces;
+using LasmartTestContext.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LasmartTestContext.Persistence;
+
+/// <inheritdoc cref="IDBContext" />
+public class DBContext : DbContext, IDBContext
+{
+    /// <summary>
+    /// Конструктор, инициализирующий настройки контекста
+    /// </summary>
+    /// <param name="options">Настройки</param>
+    public DBContext(DbContextOptions<DBContext> options): base(options) { }
+    
+    public DbSet<Point>? Points { get; set; }
+    public DbSet<Comment>? Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
