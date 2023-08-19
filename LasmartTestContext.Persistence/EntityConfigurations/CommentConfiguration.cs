a46140c8334103a7f1d12c4dba7cd0bbbaa2d83d
mipesync <arhe414@gmail.com>
2023-08-20 00:28:13 +0300

    feature: Добавил конфигурации сущностей

---
+++ LasmartTestContext.Persistence/EntityConfigurations/CommentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using LasmartTestContext.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LasmartTestContext.Persistence.EntityConfigurations;
+
+/// <summary>
+/// Конфигурация сущности <see cref="Comment"/>
+/// </summary>
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.HasIndex(u => u.Id).IsUnique();
+
+        builder.HasOne(u => u.Point)
+            .WithMany(u => u.Comments)
+            .HasForeignKey(u => u.PointId);
+    }
+}
---
+++ LasmartTestContext.Persistence/EntityConfigurations/PointConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LasmartTestContext.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LasmartTestContext.Persistence.EntityConfigurations;
+
+/// <summary>
+/// Конфигурация сущности <see cref="Point"/>
+/// </summary>
+public class PointConfiguration : IEntityTypeConfiguration<Point>
+{
+    public void Configure(EntityTypeBuilder<Point> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.HasIndex(u => u.Id).IsUnique();
+    }
+}
